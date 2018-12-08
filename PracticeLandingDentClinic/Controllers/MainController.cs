ece82a6728504b4ecf789e750b218aa85ff0e9de
Майзельс Кирилл Игоревич <@corp.itstep>
2018-12-08 14:31:29 +0600

    Добавьте файлы проекта.

---
+++ PracticeLandingDentClinic/Controllers/MainController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PracticeLandingDentClinic.Controllers
+{
+    public class MainController : Controller
+    {
+        // GET: Main
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult SendFeedback(string name, string email, string subject, string message)
+        {
+            try
+            {
+                SmtpClient client = new SmtpClient("smtp.gmail.com", 587)
+                {
+                    Credentials = new NetworkCredential("domo.ddr@gmail.com", "QazPlm3110weroiu"),
+                    EnableSsl = true
+                };
+
+                client.Send("domo.ddr@gmail.com", email, subject, message);
+            }
+            catch (Exception ex)
+            {
+                string _ex = ex.Message;
+            }
+
+            return View("Index");
+        }
+    }
+}
---
+++ PracticeLandingDentClinic/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PracticeLandingDentClinic.MvcApplication" Language="C#" %>
